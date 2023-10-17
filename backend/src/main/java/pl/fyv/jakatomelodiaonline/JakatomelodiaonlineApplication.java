982a9845108b3ad1ec8d63446a67606dbfe71737
fyv <gnowak.java@gmail.com>
2023-10-17 20:40:55 +0200

    init

---
+++ backend/src/main/java/pl/fyv/jakatomelodiaonline/JakatomelodiaonlineApplication.java
@@ -0,0 +1,13 @@
+package pl.fyv.jakatomelodiaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakatomelodiaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakatomelodiaonlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/fyv/jakatomelodiaonline/JakatomelodiaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fyv.jakatomelodiaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakatomelodiaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
